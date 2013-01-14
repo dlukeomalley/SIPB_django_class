18bc7c3e99a03c557f870dfa2121e5b11ac81117
Luke O'Malley <omalley1@mit.edu>
2013-01-13 22:48:05 -0500

    basic event functionality added

--- website/event/models.py
+++ website/event/models.py
@@ -1,4 +1,15 @@
+# standard import to get access to models.Model
 from django.db import models
+# to create a form from a model
+from django.forms import ModelForm
 
 class Event(models.Model):
-    pass
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
--- website/event/views.py
+++ website/event/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
 from django.shortcuts import render
+from event.models import Event, EventForm
 
-
+# Basic view to return just html
 def home(request):
     return render(request, 'index.html')
+
+# View to return all events
+def all_events(request):
+    events = Event.objects.all()
+    return render(request, 'events.html', {'events': events, 'form': EventForm()})
+
+def post_event(request):
+    pass
